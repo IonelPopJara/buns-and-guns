49bd09541d431b611c51ee83761e44ca112eb3da
Razvan Albu <razvan-albu@hotmail.com>
2025-04-18 22:42:58 +0200

    Fix wall overlap and corners

--- src/meshes.js
+++ src/meshes.js
@@ -5,7 +5,7 @@ const meshes = [];
 let layout = `
 +------------+
 |            |
-|  x  +-+  +-|
+|  x  +-+  +-+
 |     |      |
 +-----+      |
 |            |
@@ -23,9 +23,9 @@ function parseLayout(layout) {
   // Find the position of the 'x' character
   let startPos = null;
   for (let i = 0; i < lines.length; i++) {
-    console.log("Y: ", i);
     const line = lines[i];
     const xIndex = line.indexOf("x");
+
     if (xIndex !== -1) {
       startPos = { x: xIndex, y: i };
       break;
@@ -34,42 +34,65 @@ function parseLayout(layout) {
 
   for (let y = 0; y < lines.length; y++) {
     for (let x = 0; x < lines[y].length; x++) {
-      if (lines[y][x] === " " || lines[y][x] === "x") continue;
+      if (lines[y][x] === " " || lines[y][x] === "x") {
+        continue;
+      }
 
       const actualX = x - startPos.x;
       const actualY = y - startPos.y;
 
-      let width = 2;
-      let height = 1;
-      let color = 0xff0000;
-
       if (lines[y][x] === "+") {
-        continue;
-        width = 1;
-        height = 1;
-        color = 0x00ff00;
+        for (let position = -1; position <= 1; position++) {
+          if (y + position >= 0 &&
+            y + position < lines.length &&
+            lines[y + position][x] === "|") {
+
+            const mesh = createMesh(0.5, 1, 0x00ff00);
+
+            mesh.position.x = actualX;
+            mesh.position.z = actualY + position * 0.25;
+
+            mesh.rotation.y = Math.PI / 2;
+
+            meshes.push(mesh);
+          }
+
+          if (x + position >= 0 &&
+            x + position < lines[y].length &&
+            lines[y][x + position] === "-") {
+
+            const mesh = createMesh(0.5, 1, 0x00ff00);
+
+            mesh.position.x = actualX + position * 0.25;
+            mesh.position.z = actualY;
+
+            meshes.push(mesh);
+          }
+        }
+      } else {
+        const mesh = createMesh(1, 1, 0xff0000);
+
+        mesh.position.x = actualX;
+        mesh.position.z = actualY;
+
+        if (lines[y][x] === "|") {
+          mesh.rotation.y = Math.PI / 2;
+        }
+
+        meshes.push(mesh);
       }
-
-      const mesh = new THREE.Mesh(
-        new THREE.PlaneGeometry(width, height),
-        new THREE.MeshBasicMaterial({
-          color: color,
-          side: THREE.DoubleSide,
-          wireframe: true,
-        })
-      );
-
-      console.log("X: ", actualX, "Y: ", actualY);
-
-      mesh.position.x = actualX;
-      mesh.position.z = actualY;
-
-      if (lines[y][x] === "|") {
-        mesh.rotation.y = Math.PI / 2; // Rotate 90 degrees
-      }
-
-      meshes.push(mesh);
     }
+  }
+
+  function createMesh(width, height, color) {
+    return new THREE.Mesh(
+      new THREE.PlaneGeometry(width, height),
+      new THREE.MeshBasicMaterial({
+        color: color,
+        side: THREE.DoubleSide,
+        wireframe: true,
+      })
+    );
   }
 }
 
