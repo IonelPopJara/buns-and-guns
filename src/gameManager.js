6d4e444cb3fdcbfb643ef31fefbd56f5787f603f
Razvan Albu <razvan-albu@hotmail.com>
2025-04-20 16:20:37 +0200

    Add win screen

--- src/gameManager.js
+++ src/gameManager.js
@@ -1,6 +1,7 @@
 const pauseMenu = document.getElementById("pause-menu");
 const deathScreen = document.getElementById("death-screen");
 const homeScreen = document.getElementById("home-screen");
+const endScreen = document.getElementById("end-screen");
 const ui = document.getElementById("ui");
 
 let paused = true;
@@ -9,6 +10,7 @@ let dead = false;
 deathScreen.style.display = 'none';
 pauseMenu.style.display = 'none';
 ui.style.display = 'none';
+endScreen.style.display = 'none';
 homeScreen.style.display = 'flex';
 
 document.addEventListener("keyup", function (event) {
@@ -23,16 +25,36 @@ document.addEventListener("keyup", function (event) {
 
 document.addOnDeathHandler(() => {
     document.pauseInternal();
+
     deathScreen.style.display = 'flex';
+    pauseMenu.style.display = 'none';
     ui.style.display = 'none';
+    endScreen.style.display = 'none';
+    homeScreen.style.display = 'none';
+
+    dead = true;
+})
+
+document.addOnWinHandler(() => {
+    document.pauseInternal();
+
+    deathScreen.style.display = 'none';
+    pauseMenu.style.display = 'none';
+    ui.style.display = 'none';
+    endScreen.style.display = 'flex';
+    homeScreen.style.display = 'none';
 
     dead = true;
 })
 
 function pause() {
     document.pauseInternal();
+
+    deathScreen.style.display = 'none';
     pauseMenu.style.display = 'flex';
     ui.style.display = 'none';
+    endScreen.style.display = 'none';
+    homeScreen.style.display = 'none';
 
     paused = true;
 }
@@ -40,9 +62,12 @@ function pause() {
 function resume() {
     if (!dead) {
         document.resumeInternal();
+
+        deathScreen.style.display = 'none';
         pauseMenu.style.display = 'none';
-        homeScreen.style.display = 'none';
         ui.style.display = 'flex';
+        endScreen.style.display = 'none';
+        homeScreen.style.display = 'none';
 
         paused = false;
     }
